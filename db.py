3577ebfe0a8f8d88c1bac043ae1455448a80b88f
Jambock <jambock@users.noreply.github.com>
2018-11-15 20:32:53 +0000

    some additons to db stuff

--- db.py
+++ db.py
@@ -1,10 +1,20 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import exc
 from db_schema import User, Chat, Message
+import logging
+logger = logging.getLogger(__name__)
 
-engine = create_engine('mysql+mysqlconnector://user:pass@host/bot')
+
+engine = create_engine('mysql+mysqlconnector://root:root@172.17.0.2/bot',
+                       encoding='utf8',
+                       echo=False,
+                       echo_pool=False)
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
+Base = declarative_base()
+
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -15,26 +25,54 @@ DBSession = sessionmaker(bind=engine)
 # session.commit(). If you're not happy about the changes, you can
 # revert all of them back to the last commit by calling
 # session.rollback()
-session = DBSession()
-
-def log_message(id, chat_id, user_id, username, first_name, last_name, date, type, chat)
-
-# Insert a Person in the person table
-new_person = Person(name='new person')
-session.add(new_person)
-session.commit()
-
-# Insert an Address in the address table
-new_address = Address(post_code='00000', person=new_person)
-session.add(new_address)
-session.commit()
 
 
-'''
-    id = Column(Integer, autoincrement=False, primary_key=True)
-    chat_id = Column(Integer, ForeignKey('chat.id'), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    date = Column(TIMESTAMP, nullable=False)
-    type = Column(String(250), nullable=False)
-    chat = relationship(Chat)
-    user = relationship(User)'''
+def get_or_create(session, model, **kwargs):
+    instance = session.query(model).filter_by(**kwargs).first()
+    if instance:
+        return instance
+    else:
+        instance = model(**kwargs)
+        session.add(instance)
+        session.commit()
+        return instance
+
+
+def log_message(id, chat_id, user_id, is_bot, username, first_name, last_name, date, type, content, chat_title):
+    try:
+        session = DBSession()
+    except exc.DatabaseError as e:
+        logger.error('Failed to get DB Session')
+        pass
+    try:
+        user = get_or_create(session, User, id=user_id, is_bot=is_bot,
+                             username=username, first_name=first_name,
+                             last_name=last_name)
+        chat = get_or_create(session, Chat, id=chat_id, type=type, title=chat_title)
+        message = Message(id, date, type, content, chat, user)
+        session.add(message)
+        session.commit()
+    except (exc.SQLAlchemyError, exc.DBAPIError, exc.OperationalError, exc.DatabaseError) as e:
+        logger.error('Log message failed, rolling back')
+        session.rollback()
+    finally:
+        session.close()
+
+
+
+#log_message(session,
+#            11111, 22222, 33333, 0, 'bruno', 'bruno', 'savioli', '2018-11-14 01:00:53', 'lalala', 'content', 'test chat')
+
+
+
+#
+# # Insert a Person in the person table
+# new_person = Person(name='new person')
+# session.add(new_person)
+# session.commit()
+#
+# # Insert an Address in the address table
+# new_address = Address(post_code='00000', person=new_person)
+# session.add(new_address)
+# session.commit()
+
--- db_schema.py
+++ db_schema.py
@@ -2,10 +2,14 @@ import mysql.connector
 
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, TIMESTAMP, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+
+import logging
+logger = logging.getLogger(__name__)
+
 
 Base = declarative_base()
 
@@ -20,23 +24,44 @@ class User(Base):
     first_name = Column(String(250), nullable=False)
     last_name = Column(String(250), nullable=True)
 
+    def __init__(self, id, is_bot, username, first_name, last_name):
+        self.id = id
+        self.is_bot = is_bot
+        self.username = username
+        self.first_name = first_name
+        self.last_name = last_name
+
 
 class Chat(Base):
     __tablename__ = 'chat'
-    id = Column(Integer, autoincrement=False, primary_key=True)
+    id = Column(BigInteger, autoincrement=False, primary_key=True)
     type = Column(String(250), nullable=False)
-    title = Column(String(250), nullable=False)
+    title = Column(String(250), nullable=True)
+
+    def __init__(self, id, type, title):
+        self.id = id
+        self.type = type
+        self.title = title
 
 
 class Message(Base):
     __tablename__ = 'message'
     id = Column(Integer, autoincrement=False, primary_key=True)
-    chat_id = Column(Integer, ForeignKey('chat.id'), nullable=False)
+    chat_id = Column(BigInteger, ForeignKey('chat.id'), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
     date = Column(TIMESTAMP, nullable=False)
     type = Column(String(250), nullable=False)
+    content = Column(String(250), nullable=False)
     chat = relationship(Chat)
     user = relationship(User)
+
+    def __init__(self, id, date, type, content, chat, user):
+        self.id = id
+        self.date = date
+        self.type = type
+        self.content = content
+        self.chat = chat
+        self.user = user
 
 
 class ScheduleType(Base):
@@ -50,24 +75,64 @@ class Command(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
+    def __init__(self, name):
+        self.name = name
+
 
 class Schedule(Base):
     __tablename__ = 'schedule'
     id = Column(Integer, primary_key=True)
-    chat_id = Column(Integer, ForeignKey('chat.id'), nullable=False)
+    chat_id = Column(BigInteger, ForeignKey('chat.id'), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
-    schedule_type_id = Column(Integer, ForeignKey('schedule_type.id'), nullable=False)
     name = Column(String(250), nullable=False)
+    schedule_type_id = Column(Integer, ForeignKey('schedule_type.id'), nullable=False)
     command_id = Column(Integer, ForeignKey('command.id'), nullable=False)
     command_arguments = Column(String(250), nullable=False)
-    content = Column(String(250), nullable=False)
+    content = Column(String(250), nullable=True)
+    interval = Column(Integer, nullable=False)
     chat = relationship(Chat)
     user = relationship(User)
     command = relationship(Command)
+    schedule_type = relationship(ScheduleType)
+
+    def __init__(self, chat, user, name, schedule_type, interval, command, command_arguments, content):
+        self.chat = chat
+        self.user = user
+        self.name = name
+        self.schedule_type = schedule_type
+        self.interval = interval
+        self.command = command
+        self.command_arguments = command_arguments
+        self.content = content
 
 
 
 
-engine = create_engine('mysql+mysqlconnector://user:pass@host/bot')
-# Base.metadata.drop_all(engine)
-Base.metadata.create_all(engine)
+
+def main():
+
+    engine = create_engine('mysql+mysqlconnector://root:root@172.17.0.2/bot',
+                           encoding='utf8',
+                           echo=True,
+                           echo_pool=True)
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+    from sqlalchemy.orm import sessionmaker
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    commands = ['chuva', 'reminder']
+    for c in commands:
+        command = Command(name=c)
+        session.add(command)
+    session.commit()
+
+    schedule_type = ['run_once', 'run_repeating', 'run_daily']
+    for st in schedule_type:
+        schedule_type = ScheduleType(type=st)
+        session.add(schedule_type)
+    session.commit()
+
+
+if __name__ == "__main__":
+    main()
